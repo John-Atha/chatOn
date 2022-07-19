5a5ee06f5b9a039abd07412fe32c18e317b06012
John-Atha <giannisj3@gmail.com>
2022-07-19 16:20:39 +0300

    Update

--- backend/helpers/getRequestUser.js
+++ backend/helpers/getRequestUser.js
@@ -3,6 +3,7 @@ const JsonWebToken = require("jsonwebtoken");
 const getRequestUser = (req) => {
     const { headers } = req;
     const bearer = headers['authorization'];
+    console.log({ bearer })
     if (!bearer?.includes('Bearer ')) {
         return null;
     }
